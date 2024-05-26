049b1e0ac71db9edd309fed57e88ce1c4f9caab7
mkozhemiakin134 <matveyko662@gmail.com>
2024-05-26 17:12:19 +0300

    Update tdrytfugyiuhop.py

--- tdrytfugyiuhop.py
+++ tdrytfugyiuhop.py
@@ -1,104 +1,108 @@
 from pygame import *
-font.init()
-font1 = font.SysFont("Arial", 36)
+'''Необходимые классы'''
 
-from time import time as timer 
 
-#создай окно игры
-win_width = 900
-win_height = 700
-game_win = display.set_mode((win_width, win_height))
-display.set_caption("Пинг-понг")
-
-#задай фон сцены
-background = image.load("table.jpg")
-background = transform.scale(background, (win_width,win_height))
-clock = time.Clock()
-FPS = 60
-
-dx = 5
-dy = 5
-
+#класс-родитель для спрайтов
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))    
+        self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
-        self.rect.y = player_y 
-        self.size_x = size_x
-        self.size_y = size_y
-    def reset(self):
-        game_win.blit(self.image, (self.rect.x, self.rect.y))
+        self.rect.y = player_y
 
-    def colliderect(self, rect):
-        return self.rect.colliderect(rect)
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 
 class Player(GameSprite):
-    def update_right(self):
+    def update_r(self):
         keys = key.get_pressed()
-        
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 5:
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-
-    def update_left(self):
+    def update_l(self):
         keys = key.get_pressed()
-        
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 5:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-#обработай событие «клик по кнопке "Закрыть окно"»
-rocket1 = Player("platform.png", win_width - 20, win_height // 2 - 50, 20, 100, 10)
-rocket2 = Player("platform.png", 5, win_height // 2 - 50, 20, 100, 10)
 
-class Ball(GameSprite):
-    def update(self):
-        global dx, dy
-        self.rect.x += dx
-        self.rect.y += dy
+#игровая сцена:
+back = (200, 255, 255) #цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
 
-        if self.rect.y <= 0 or self.rect.y > 650:
-            dy = dy * (-1)
-        
-        if self.rect.x > 890:
-            self.rect.x = win_width // 2
-            self.rect.y = win_height // 2
 
-        if  self.rect.x < 10:
-            self.rect.x = win_width // 2
-            self.rect.y = win_height // 2
-
-        if self.colliderect(rocket1.rect) or self.colliderect(rocket2.rect):
-            dx = dx * (-1)
-
-ball = Ball("ball.png", win_width // 2, win_height // 2, 50, 50, 7)
-
-finish = False
+#флаги, отвечающие за состояние игры
 game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+#создания мяча и ракетки   
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
+speed_x = 3
+speed_y = 3
+
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
+    
     if finish != True:
-        game_win.blit(background, (0,0))
-        rocket1.update_right()
-        rocket1.reset()
-        rocket2.update_left()
-        rocket2.reset()
-        ball.update()
-        ball.reset()
-
-    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
-      
+
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        
+        #если мяч достигает границ экрана, меняем направление его движения
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+
+       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
 
     display.update()
     clock.tick(FPS)
